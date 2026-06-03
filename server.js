2645497e9a65f371d24ba03b3349a2370d779dfe
five890 <161637917+five890@users.noreply.github.com>
2026-06-03 14:40:27 -0300

    Refactor server.js to use environment port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+
+const app = express();
 
 app.use(express.static(__dirname));
 
@@ -8,6 +9,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
