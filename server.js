0000bd78234df654df6fd984a4a59e23f7dd2232
five890 <161637917+five890@users.noreply.github.com>
2026-06-03 14:37:23 -0300

    Refactor server.js to serve static files

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
+const path = require("path");
+
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.send("Servidor rodando ✔");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
 });
